473a446d5e9a7048658d3e3314987b2d1dfc7de1
Scott Schlegel <schlegz76@gmail.com>
2018-07-29 16:26:37 -0700

    add paginate subscription and count method

--- auditlog.js
+++ auditlog.js
@@ -1,6 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { diff } from 'deep-diff'; // eslint-disable-line import/no-unresolved
-
+import { check, Match } from 'meteor/check';
 import AuditLogSchema from './schema/auditlog';
 
 const AuditLogs = new Mongo.Collection('auditlog');
@@ -84,6 +84,51 @@ if (Meteor.isServer) {
     return AuditLogs.find({}, { sort: { createdAt: -1 } });
   });
 
+  Meteor.publish('auditlogs.paginate', function (sort, limit, search) {
+    check(sort, Object);
+    check(limit, Number);
+    check(search, Match.OneOf(String, null, undefined));
+
+    // if not logged in, indicate that we are done and have sent everything
+    if (!this.userId) {
+      return this.ready();
+    }
+
+    let query = {};
+    const projection = { sort, limit };
+
+    if (search) {
+      const regex = new RegExp(search, 'i');
+
+      query = {
+        $or: [
+          { name: regex },
+        ],
+      };
+    }
+
+    return AuditLogs.find(query, projection);
+  });
+
+  Meteor.methods({
+    'auditlogs.count'({ search }) {
+      if (!this.isSimulation) {
+        let query = {};
+        const regex = new RegExp(search, 'i');
+
+        // query = {
+        //   $or: [
+        //     { name: regex },
+        //   ],
+        // };
+
+        return AuditLogs.find(query).count();
+      }
+
+      return 0;
+  }
+});
+
   /**
    * Attach event handler to the collection to log add/remove/update
    * modifications to documents.
@@ -112,6 +157,19 @@ if (Meteor.isServer) {
 } else {
   AuditLogs.subscribeAll = function () {
     return Meteor.subscribe('auditlogs.all');
+  };
+
+  AuditLogs.subscribePaginate = function (sort, limit, search) {
+    return Meteor.subscribe(
+      'auditlogs.paginate',
+      sort,
+      limit,
+      search,
+    );
+  };
+
+  AuditLogs.count = function (search, callback) {
+    return Meteor.call('auditlogs.count', { search }, callback);
   };
 }
 
